279dddd3e14f58d86e52fd0f53e5284985729908
Aseliyaa <104303919+Aseliyaa@users.noreply.github.com>
2024-04-23 17:19:56 +0300

    Update Router.java
    login and register pages

--- src/main/java/com/example/demo/command/Router.java
+++ src/main/java/com/example/demo/command/Router.java
@@ -1,2 +1,30 @@
-package com.example.demo.command;public class Router {
+package com.example.demo.command;
+
+public class Router {
+    private String page = "index.jsp";
+    private Type type = Type.FORWARD;
+    enum Type{
+        FORWARD, REDIRECT;
+    }
+
+    public Router(String page) {
+        this.page = page;
+    }
+
+    public Router(String page, Type type) {
+        this.page = page;
+        this.type = type;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public void setRedirect(Type type) {
+        this.type = Type.REDIRECT;
+    }
 }
